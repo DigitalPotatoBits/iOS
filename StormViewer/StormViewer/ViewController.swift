b801ba0ecb4757c056b4a68d0f87bf486ab41bdc
Joshua Baker <krrsplat@gmail.com>
2021-04-14 17:15:11 -0500

    First commit for the storm viewer app.

---
+++ StormViewer/StormViewer/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  StormViewer
+//
+//  Created by Joshua Baker on 4/13/21.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    var pictures = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let fileManager = FileManager.default // the shared file manager for this process.
+        let path = Bundle.main.resourcePath!
+        let items = try! fileManager.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        
+//        for index in 0 ..< pictures.count {
+//
+//        }
+        
+        print(pictures)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+}
+
