3fb9b5d31adc207ebdf97c707c6c08530efe91ae
Joshua Baker <krrsplat@gmail.com>
2021-04-17 23:23:15 -0500

    Finished my first tiny iOS project.

--- StormViewer/StormViewer/ViewController.swift
+++ StormViewer/StormViewer/ViewController.swift
@@ -13,22 +13,11 @@ class ViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        let fileManager = FileManager.default // the shared file manager for this process.
-        let path = Bundle.main.resourcePath!
-        let items = try! fileManager.contentsOfDirectory(atPath: path)
         
-        for item in items {
-            if item.hasPrefix("nssl") {
-                pictures.append(item)
-            }
-        }
+        navigationController?.navigationBar.prefersLargeTitles = true
+        title = "Storm Viewer"
         
-//        for index in 0 ..< pictures.count {
-//
-//        }
-        
-        print(pictures)
+        loadPicturesFromAssets()
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -39,6 +28,28 @@ class ViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
         cell.textLabel?.text = pictures[indexPath.row]
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            vc.imageTitle = "\(indexPath.row + 1) of \(pictures.count)"
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
+    func loadPicturesFromAssets() {
+        let fileManager = FileManager.default // the shared file manager for this process.
+        let path = Bundle.main.resourcePath!
+        let items = try! fileManager.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        
+        pictures.sort()
     }
 }
 
