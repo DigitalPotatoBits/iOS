59b5d620947722cc57b343168ca4e5b33f0d7037
Joshua Baker <krrsplat@gmail.com>
2021-04-21 21:06:42 -0500

    Started new flag guessing project.

---
+++ GuessTheFlag/GuessTheFlag/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GuessTheFlag
+//
+//  Created by Joshua Baker on 4/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
