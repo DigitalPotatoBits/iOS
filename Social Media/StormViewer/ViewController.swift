43fbdefbb281a08e6c2af3b816697532e7feaf62
Joshua Baker <thepotatoprogrammer@gmail.com>
2021-04-22 22:14:48 -0500

    new commit, bad message.

---
+++ Social Media/StormViewer/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  StormViewer
+//
+//  Created by Joshua Baker on 4/13/21.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    var pictures = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        title = "Storm Viewer"
+        
+        loadPicturesFromAssets()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            vc.imageTitle = "\(indexPath.row + 1) of \(pictures.count)"
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
+    func loadPicturesFromAssets() {
+        let fileManager = FileManager.default // the shared file manager for this process.
+        let path = Bundle.main.resourcePath!
+        let items = try! fileManager.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        
+        pictures.sort()
+    }
+}
+
